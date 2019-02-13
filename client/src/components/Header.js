b92eb2f3ad5fb32515fc8f76bc7d8f247a9b507a
isimsek <isimsek@megazebra.com>
2019-02-13 15:17:37 +0100

    simple styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import './HeaderStyle.css';
 
 class Header extends Component {
   renderLinks() {
@@ -20,7 +20,7 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
+      <div className="header">
         <Link to="/">Redux Auth</Link>
         { this.renderLinks() }
       </div>
