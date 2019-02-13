4d6723d344d2be7443753a7cb93345ace778f9ae
isimsek <isimsek@megazebra.com>
2019-02-13 14:11:41 +0100

    signup auth reducers/actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,19 @@
-import { AUTH_USER } from 'actions/types';
+import { AUTH_USER, AUTH_ERROR } from 'actions/types';
 import axios from 'axios';
 
 export const signup = (formProps) => async dispatch => {
-  const response = await axios.post('http://localhost:3090/signup', formProps);
+  try {
+    const response = await axios.post('http://localhost:3090/signup', formProps);
 
-  dispatch({
-    type: AUTH_USER,
-    payload: response.data.token
-  });
+    dispatch({
+      type: AUTH_USER,
+      payload: response.data.token
+    });
+  } catch (e) {
+    dispatch({
+      type: AUTH_ERROR,
+      payload: 'Email in use'
+    });
+  }
+
 };
