2a4bc8b0c209c2463138c91b6c1b9312657642b3
isimsek <isimsek@megazebra.com>
2019-02-13 14:34:01 +0100

    redirection after signup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
-import { AUTH_USER, AUTH_ERROR } from 'actions/types';
+import { AUTH_ERROR, AUTH_USER } from 'actions/types';
 import axios from 'axios';
 
-export const signup = (formProps) => async dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post('http://localhost:3090/signup', formProps);
 
@@ -9,6 +9,8 @@ export const signup = (formProps) => async dispatch => {
       type: AUTH_USER,
       payload: response.data.token
     });
+
+    callback();
   } catch (e) {
     dispatch({
       type: AUTH_ERROR,
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -6,7 +6,9 @@ import { Field, reduxForm } from 'redux-form';
 
 class Signup extends Component {
   onSubmit = (formProps) => {
-    this.props.signup(formProps);
+    this.props.signup(formProps, () => {
+      this.props.history.push('/feature');
+    });
   };
 
   render() {
