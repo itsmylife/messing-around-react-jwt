85ebf1a5a161d18efeeb44b63c70882c42d466d7
isimsek <isimsek@megazebra.com>
2019-02-13 12:50:38 +0100

    async signup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,11 @@
+import { AUTH_USER } from 'actions/types';
 import axios from 'axios';
 
-export const signup = (formProps) => dispatch => {
-  axios.post('http://localhost:3090/signup', formProps);
+export const signup = (formProps) => async dispatch => {
+  const response = await axios.post('http://localhost:3090/signup', formProps);
+
+  dispatch({
+    type: AUTH_USER,
+    payload: response.data.token
+  });
 };
